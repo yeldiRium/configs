f3bce72cc452b2cd0cc886c04e03c52fef8cacf2
Hannes Leutloff <hannes.leutloff@aol.de>
2018-03-28 13:12:23 +0200

    replace npm package.json with install script

---
+++ npm-global/npm-install-globals.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# please run with sudo
+# also execute 'pacman -s semver' first
+npm i -g npm
+npm i -g babel-cli
+npm i -g bower
+npm i -g browserify
+npm i -g eslint
+npm i -g express-generator
+npm i -g gulp
+npm i -g http-server
+npm i -g node-sass --unsafe-perm # to fix permissions issue https://github.com/sass/node-sass/issues/2006
+npm i -g nodemon
+npm i -g require
+npm i -g rollup
+npm i -g semver
+npm i -g vue-cli
+npm i -g webpack
+npm i -g webpack-cli
