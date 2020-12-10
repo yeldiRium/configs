f2968bbd15931c3a83593cfaff4e8d928baba7d4
Hannes Leutloff <hannes.leutloff@yeldirium.de>
2020-12-10 17:46:15 +0100

    [zsh] fix neuron aliases

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -29,9 +29,9 @@ if [ -x "$(command -v kubectl)" ]; then
 fi
 
 # neuron (https://github.com/srid/neuron)
-alias zk-se="code $(neuron search)"
+alias zk-se='code $(neuron search)'
 alias zk-sc="neuron search | rev | cut -d '/' -f 1 | rev | cut -d '.' -f 1 | xclip -sel clip -r"
-alias zk-st="neuron query | jq -r '.result[].zettelTags[]' | uniq | dmenu | xclip -sel clip -r"
+alias zk-st="neuron query | jq -r '.result[].zettelTags[]' | sort | uniq | dmenu | xclip -sel clip -r"
 
 zk-qt() {
     if [ ! -n $1 ]
